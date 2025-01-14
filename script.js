1c31e8d3901c62c7dd1bc54312fe6d52e2890ccc
Nirmal Odedara <nirmalodedara007@gmail.com>
2025-01-14 11:27:29 +0530

    add leaflet map on web app and appdate location pin accoding to ISS location

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 const issId = 25544 // id of ISS
 const API = `https://api.wheretheiss.at/v1/satellites/${issId}` // API forget info of ISS from 'where the ISS at?'
 
-
-setInterval(setLatLon, 1000)
+let first = true
+let marker
+ setInterval(setLatLon, 1000)
 
 async function getData(){
-
     const response  = await fetch(API)
     const data = response.json()
-    
     return data
 } 
 
@@ -16,12 +15,28 @@ async function setLatLon(){
    
     const data = await getData()
 
-    
+    const latLong = [data.latitude, data.latitude]
     document.querySelector("#latitude").textContent = data.latitude
     document.querySelector("#longitude").textContent = data.longitude
+    
+    if(first){
+        let map = L.map('map').setView(latLong, 9);
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+        marker = L.marker([51., -0.09]).addTo(map);
+        first = false
+    }else{
+        marker.setLatLng(latLong)
+    }
 
+    
 
+     
 }
+
+
 
 
 
